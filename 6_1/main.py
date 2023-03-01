ce880d8ee38a437b466bf55b0b6dd0a302cb26aa
DmitryBal <76563919+DmitryBal@users.noreply.github.com>
2023-03-01 17:48:25 +0300

    Create main.py

---
+++ 6_1/main.py
@@ -0,0 +1,53 @@
+import numpy as np
+from func import sub, printMatrix, determinant, max_elem
+
+SLAY = np.array([[-2.0, -1.0, -9.0, -5.0, 93.0],
+                 [-4.0, 4.0, -2.0, 6.0, 16.0],
+                 [0.0, 5.0, 7.0, -4.0, -80.0],
+                 [0.0, 9.0, 7.0, 7.0, -119.0]])
+n = len(SLAY)
+
+
+# Перестановка текущей строки со строкой, содержащей опорный элемент
+def swap(matrix, k):
+    M = np.delete(matrix, np.s_[0:k], axis=0)
+    elem = max_elem(M, k)
+    # если r(A) < r(n)
+    if elem == 0:  
+        print("CЛАУ несовместна. Решений нет")
+        quit(1)
+    for i in range(k, len(matrix)):
+        if matrix[i][k] == elem and i != k:
+            for j in range(len(matrix[i])):
+                matrix[i][j], matrix[k][j] = matrix[k][j], matrix[i][j]
+    return matrix
+
+
+# Приведение к треугольной матрице
+def triangle(matrix):
+    for j in range(n):
+        matrix = swap(matrix, j)
+        for i in range(j + 1, n):
+            c = matrix[i][j] / matrix[j][j]
+            matrix = sub(matrix, i, j, c)
+            printMatrix(matrix)
+    return matrix
+
+
+# Метод Гаусса
+def gauss(matrix):
+    matrix = triangle(matrix)
+    det = determinant(matrix)
+    if det != 0:
+        b = matrix[:, n]
+        a = np.delete(matrix, np.s_[n:n + 1], axis=1)
+        x = np.linalg.solve(a, b)
+        for i in range(n):
+            print(f'x{i + 1} = ', x[i])
+    else:
+        print("СЛАУ вырождена, т.к. определитель равен нулю")
+
+
+if __name__ == '__main__':
+    printMatrix(SLAY)
+    gauss(SLAY)
