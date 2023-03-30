89f0a1f0783cf4299bb2663c4016930a61da6f75
DmitryBal <76563919+DmitryBal@users.noreply.github.com>
2023-03-30 16:01:40 +0300

    Update 6_4.py

--- 6_4/6_4.py
+++ 6_4/6_4.py
@@ -3,12 +3,12 @@ from sympy import diff, symbols
 from numpy import cos, sin, log10 as lg, log as ln, exp as e
 
 epsilon = 0.001
-f_x = 'x^6 - 5*x - 2'
 t = 2.5
 t_0 = 2.0
 
 
 ab = np.array([1.0, 2.0])
+f_x = 'x^6 - 5*x - 2'
 # Приближение функции f(x) = x^6 - 5x - 2
 def f_conv(x):
     return (5*x + 2)**(1/6)
