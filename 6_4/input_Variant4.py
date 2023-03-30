0c097bcd3ad78fb0bf512273a87ae0fbeeab877a
DmitryBal <76563919+DmitryBal@users.noreply.github.com>
2023-03-30 16:31:04 +0300

    Update input_Variant4.py

--- 6_4/input_Variant4.py
+++ 6_4/input_Variant4.py
@@ -1,5 +1,5 @@
 ab = np.array([1.0, 2.0])
 f_x = 'sin(x) - x^2 + 1'
-# Приближение функции f(x) = x^6 - 5x - 2
+# Приближение функции f(x) = sin(x) - x^2 + 1
 def f_conv(x):
     return np.sqrt(sin(x) + 1.0)
