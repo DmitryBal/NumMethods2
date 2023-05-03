73d2b78611a68fe125fbfeeb77a5b491d5cfd186
DmitryBal <76563919+DmitryBal@users.noreply.github.com>
2023-05-03 13:50:41 +0300

    Update main.py

--- 6_1/main.py
+++ 6_1/main.py
@@ -13,7 +13,7 @@ def swap(matrix, k):
     M = np.delete(matrix, np.s_[0:k], axis=0)
     elem = max_elem(M, k)
     # если r(A) < r(n)
-    if elem == 0:  
+    if elem == 0:
         print("CЛАУ несовместна. Решений нет")
         quit(1)
     for i in range(k, len(matrix)):
@@ -34,14 +34,29 @@ def triangle(matrix):
     return matrix
 
 
+def searchSolution(a):
+    n = len(a)
+    print('Обратный ход Гауса:\n')
+    printMatrix(a)
+    for i in range(n-1,-1,-1):
+        a[i]/=a[i][i]
+        if i < n-1:
+            a[i][n] -= sum([a*b for a,b in zip(a[i+1:n, n],a[i][i+1:n])])
+            a[i][i+1:n] = 0
+        printMatrix(a)
+    return a[:,n]
+
 # Метод Гаусса
 def gauss(matrix):
     matrix = triangle(matrix)
     det = determinant(matrix)
     if det != 0:
+        '''
         b = matrix[:, n]
         a = np.delete(matrix, np.s_[n:n + 1], axis=1)
-        x = np.linalg.solve(a, b)
+        x = np.linalg.solve(a, b)  
+        '''
+        x = searchSolution(matrix)
         return x
     else:
         print("СЛАУ вырождена, т.к. определитель равен нулю")
